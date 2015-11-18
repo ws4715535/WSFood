88bba1dde92739724486487517deb2665fee6d69
ws <ws@wsdemac-pro.local>
2015-11-18 18:08:21 +0800

    初始化项目

---
+++ textGit/textGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textGit
+//
+//  Created by Ws on 15/11/18.
+//  Copyright © 2015年 Ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ textGit/textGit/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  textGit
+//
+//  Created by Ws on 15/11/18.
+//  Copyright © 2015年 Ws. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+    NSLog(@"123");
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
